71d659db1c70e0fadceb2d7ded7f5df79105b74b
Hakan Bilgi <hknblg34@gmail.com>
2022-09-28 09:46:45 +0300

    app

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
 const input2 = document.querySelector(".input2");
 const input = document.querySelector(".input");
 const btnContainer = document.querySelector(".button-container");
+
+let num1;
+let num2;
+let operator;
